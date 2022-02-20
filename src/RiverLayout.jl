47a2bfbe898dcecfbc06f1e706db139a5ef3bb61
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-02-20 22:56:30 +0800

    Add code and header generation from scanner

--- src/RiverLayout.jl
+++ src/RiverLayout.jl
@@ -1,5 +1,21 @@
 module RiverLayout
 
-greet() = print("Hello World!")
+export generate
 
-end # module
+using Wayland_jll
+using Wayland_protocols_jll
+
+function generate()
+  project_root = rstrip(normpath(joinpath(Base.current_project()), "../"), '/')
+
+  Wayland_jll.wayland_scanner() do exe
+    @info "Generating private code"
+    run(`$exe private-code "$project_root/protocols/river-layout-v3.xml" "$project_root/src/wayland/river-layout-v3-protocol.c"`)
+  end
+
+  Wayland_jll.wayland_scanner() do exe
+    @info "Generating client header"
+    run(`$exe client-header "$project_root/protocols/river-layout-v3.xml" "$project_root/src/wayland/river-layout-v3-protocol.h"`)
+  end
+end
+end
